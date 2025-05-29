d6782b680afa801a559d2a68bb3ca35a36a957ee
William Price <willvrd88@gmail.com>
2025-05-29 16:17:35 -0700

    updates

--- lua/kickstart/plugins/git-linker.lua
+++ lua/kickstart/plugins/git-linker.lua
@@ -1,3 +1,22 @@
 return {
-  'ruifm/gitlinker.nvim',
+  'linrongbin16/gitlinker.nvim',
+  opts = {},
+  keys = {
+    {
+      '<leader>gl',
+      function()
+        require('gitlinker').link()
+      end,
+      mode = { 'n', 'v' },
+      desc = '[g]it [l]ink',
+    },
+    {
+      '<leader>gb',
+      function()
+        require('gitlinker').link { router_type = 'blame' }
+      end,
+      mode = { 'n', 'v' },
+      desc = '[g]it [b]lame',
+    },
+  },
 }
--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -42,9 +42,13 @@ require('lazy').setup({
   require 'kickstart/plugins/treesitter',
 
   require 'kickstart/plugins/oil',
-  -- require 'kickstart/plugins/rose-pine',
-  require 'kickstart/plugins/catppuccin',
 
+  require 'kickstart.plugins.lualine',
+  require 'kickstart.plugins.catppuccin',
+  require 'kickstart.plugins.hardtime',
+  require 'kickstart.plugins.git-linker',
+
+  require 'kickstart.plugins.yaml',
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
@@ -54,12 +58,11 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
-
+  require 'kickstart.plugins.avante'
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
